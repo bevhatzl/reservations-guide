99a8ef0f5b77ee7689568882d4fbc24976ce733c
bevhatzl <beverleyhatzl@gmail.com>
2021-01-31 17:23:04 +1100

    search not working

--- controllers/blacklistController.js
+++ controllers/blacklistController.js
@@ -11,16 +11,16 @@ module.exports = {
   },
 
   find: function(req, res) {
+    console.log(req.params)
     Blacklist
     .find({ 
       $or: [
         { $and: [{guest_name: req.params.guest_name}, {guest_DOB: req.params.guest_DOB}]},
         { $or: [{guest_ID_num: req.params.id_search}, {ch_ID_num: req.params.id_search}]}
-      ]})
-    
-      .sort({entry_date: 'descending'})
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      ]})    
+    .sort({entry_date: 'descending'})
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
   },
   
 };
--- routes/api/blacklist-routes.js
+++ routes/api/blacklist-routes.js
@@ -5,7 +5,7 @@ const blacklistController = require("../../controllers/blacklistController");
 router.route("/api/blacklist")
   .post(blacklistController.create);
 
-  router.route("/api/blacklists")
+router.route("/api/blacklists")
   .get(blacklistController.find);
 
-  module.exports = router;
+module.exports = router;
