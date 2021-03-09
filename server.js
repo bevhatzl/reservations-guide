7c83f0d9b398107de165be60a8445273b058002a
bevhatzl <beverleyhatzl@gmail.com>
2021-03-09 21:34:36 +1100

    update mongoose connection for deployed version

--- server.js
+++ server.js
@@ -41,19 +41,23 @@ const mongoURI = `mongodb+srv://${USERID}:${PWD}@cluster0.fqf9a.mongodb.net/rese
 // const mongoURI = "mongodb+srv://trilogy:trilogy@cluster0.u8m9s.mongodb.net/mernlogin?retryWrites=true&w=majority";
 
 // Connect to MongoDB
-mongoose.connect( MONGODB_URI || mongoURI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-})
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/reservations-guide',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-//   }
-// )
+
+// For local
+// mongoose.connect( MONGODB_URI || mongoURI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// })
+
+// For deploying
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/reservations-guide',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+)
 .then(() => console.log("MongoDB successfully connected"))
 .catch(err => console.log(err));
 
